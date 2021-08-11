5534e8116f18d6018474eaf097baa596b3e3e50d
Chirstian Reyes <christianreyesy@hotmail.com>
2021-08-10 17:41:11 -0700

    Agregar GETS

---
+++ modules/info_participante.py
@@ -0,0 +1,39 @@
+from os import environ, urandom
+import hashlib
+import datetime
+from bottle import response, request
+import jwt
+import binascii
+from functools import wraps
+import models.auth as mauth
+
+
+
+
+
+
+
+import json
+from modules.storage import (
+    store_string,
+    store_bytes,
+    query_storage,
+    get_storage_file
+)
+
+
+#
+# ...
+#
+
+def almacenar_datos(id_participante=None, nombre=None, edad=None, domicilio=None,colonia=None):
+    print("Desde modulo almacenar_datos")
+    print(id_partipante,nombre, edad, domicilio,colonia)
+    para_almacenar = {"id_partipante": id_partipante,"nombre": nombre, "edad": edad, "domicilio":domicilio, "colonia" :colonia}
+    nombre_de_archivo = f"{id_partipante}-{nombre}.json"
+    datos = store_string(
+        "vg_info/Carpeta",
+        nombre_de_archivo,
+        json.dumps(para_almacenar)
+    )
+    return datos
