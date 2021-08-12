1db42de43280a89f785f81b44fe6ec0de6b24a84
Chirstian Reyes <christianreyesy@hotmail.com>
2021-08-12 14:27:31 -0700

    agregar entidad de eventos, modificacion en la codificacion dentro de routes

--- routes/chriscroos.py
+++ routes/chriscroos.py
@@ -2,7 +2,7 @@ import bottle
 import datetime as dt
 from modules.bottles import BottleJson
 from modules.chriscross import guardar_participante
-
+from modules.chriscross import guardar_eventos
 app = BottleJson()
 
 @app.post("/participante")
@@ -14,7 +14,7 @@ def post_de_participante():
     curl http://localhost:8080/chriscross/participante \
         -X POST \
         -H "Content-Type: application/json" \
-        -d '{"nombre":"foo", "edad": 1, "fecha": "2021-01-10"}'
+        -d '{"id_part":10,nombre":"foo", "edad": 1, "fecha": "2021-01-10"}'
     ```
     """
     try:
@@ -43,3 +43,43 @@ def post_de_participante():
         raise bottle.HTTPError(500, "Error en los datos")
     # si no hay fallo aqui continua el codigo.
     raise bottle.HTTPError(201, "Registrado")
+
+    @app.post("/evento")
+    def post_de_evento():
+        """
+        Ejemplo de solicitud con curl
+
+        ```
+        curl http://localhost:8080/chriscross/evento \
+            -X POST \
+            -H "Content-Type: application/json" \
+            -d '{"nombre":"foo", "edad": 1, "fecha": "2021-01-10"}'
+        ```
+        """
+        try:
+            # Aqui codificacmos toda la logica que pueda
+            # fallar debido a malformaciones de datos
+            # proporcinados por el usuario.
+
+            payload = bottle.request.json
+            id_evento = payload["id_evento"]
+            nombre_evento = payload["nombre_evento"]
+            distancia=payload["distancia"]
+
+            # uso de la funcion `dt.date.fromisoformat`
+            # para interpetar la fecha a de formato iso.
+            fecha = dt.date.fromisoformat(payload["fecha"])
+            # llamada a la funcion asistente en
+            # `modules/chriscross.py`.
+            guardar_participante(
+                id_evento=id_evento,
+                nombre_evento=nombre_evento,
+                distancia=distancia,
+                fecha=fecha.isoformat(), # codificacion de la fecha a cadena de texto en format iso.
+            )
+        except Exception as e:
+            # bloque que se ejecuta cuando ocurre algun fallo
+            print(e)
+            raise bottle.HTTPError(500, "Error en los datos")
+        # si no hay fallo aqui continua el codigo.
+        raise bottle.HTTPError(201, "Registrado")
